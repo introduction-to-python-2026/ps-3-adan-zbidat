f7687bd69c071ad46cc63067a3a28b4e681eb845
adan-zbidat <adanzbidat2006@gmail.com>
2025-11-12 21:42:22 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -2,7 +2,7 @@ def approximate_pi(n_terms):
     t=0
     for n in range(n_terms):
      t+=(-1)**n/(2*n+1)
-     approximate_pi=t*4
-     return approximate_pi
+    approximate_pi=t*4
+    return approximate_pi
 
   
