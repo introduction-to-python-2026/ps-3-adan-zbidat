f154e3d1bf7424a073d1c6e8c5e55d083182cf1d
adan-zbidat <adanzbidat2006@gmail.com>
2025-11-12 20:31:57 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,16 +1,16 @@
 def approximate_pi(n_terms):
-    list_of_alternating_signs = []
-    list_of_numbers = []
+    list_of_alternating_signs=[]
+    list_of_numbers=[]
     for i in range(n_terms):
         list_of_numbers.append(i)
-        if i % 2 == 0:
+        if i % 2==0:
             list_of_alternating_signs.append(1)
         else:
             list_of_alternating_signs.append(-1)
 
-    leibniz_series = []
+    leibniz_series=[]
     for i in range(n_terms):
-        leibniz_series.append(list_of_alternating_signs[i] * (1 / (2 * list_of_numbers[i] + 1)))
+        leibniz_series.append(list_of_alternating_signs[i]*(1/(2*list_of_numbers[i]+1)))
 
-    return sum(leibniz_series) * 4
+    return sum(leibniz_series)*4
     
